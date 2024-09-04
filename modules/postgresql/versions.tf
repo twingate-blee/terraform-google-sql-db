e804a3073924c4b6035995a717d8d848d46ea592
Bob Lee <bob@twingate.com>
2024-09-04 18:39:24 -0400

    update version

--- modules/postgresql/versions.tf
+++ modules/postgresql/versions.tf
@@ -27,11 +27,11 @@ terraform {
     }
     google = {
       source  = "hashicorp/google"
-      version = "= 6.0.1"
+      version = "6.0.1"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "= 6.0.1"
+      version = "6.0.1"
     }
   }
 
